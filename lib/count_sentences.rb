5e2d44825349f86fb5b681c1616ef6adb3beaddb
Ricky Rojas <rickyrojas19@gmail.com>
2019-05-06 20:42:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else 
-      return false
-    end
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
